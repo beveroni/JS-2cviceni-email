b1f7c6aaa3cf0e0a4346315958992ad835636833
Veronika Bendova <svechovaver@gmail.com>
2023-07-28 22:05:34 +0200

    Task done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 console.log('funguju');
+
+const goodbye = (name) => {
+  return 'S pozdravem ' + name;
+};
+
+const formalGodbye = (name) => {
+  return 'S uctivou poklonou' + name;
+};
+
+const nonFormalGoodbye = (name) => {
+  return 'Tak zdař Bůh' + name;
+};
+
+const fillSubject = (subject) => {
+  document.querySelector('.email__subject').textContent = subject;
+};
+
+const fillBody = (body, name, goodbyeFunction) => {
+  const bodyElm = document.querySelector('.email__body');
+  bodyElm.innerHTML = body;
+  const closingElm = document.querySelector('.email__closing');
+  closingElm.textContent = goodbyeFunction(name);
+};
